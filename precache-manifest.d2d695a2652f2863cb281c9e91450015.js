1d9a5c23b565bc5537befa5b00103cfa32ceb2d4
107062318 <karta15025452@gmail.com>
2020-06-02 19:17:32 +0800

    Updates

---
+++ precache-manifest.d2d695a2652f2863cb281c9e91450015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aef2125139cf0cc7e9fa3453c28942f",
+    "url": "/karta1502545/my-first-react/index.html"
+  },
+  {
+    "revision": "add8755812bc5d7b6469",
+    "url": "/karta1502545/my-first-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "54837766cedcef7231c5",
+    "url": "/karta1502545/my-first-react/static/js/2.1072a7e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karta1502545/my-first-react/static/js/2.1072a7e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add8755812bc5d7b6469",
+    "url": "/karta1502545/my-first-react/static/js/main.4261b7f3.chunk.js"
+  },
+  {
+    "revision": "2d76aa8a7d619fe5e848",
+    "url": "/karta1502545/my-first-react/static/js/runtime-main.b21cf2d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/karta1502545/my-first-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
