4851685c29b4c8baa290b0670ca97e55f6c38cdf
107062318 <karta15025452@gmail.com>
2020-06-02 20:34:44 +0800

    first use template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import Navigation from '.components/Navigation'
-import About from '.components/About'
-import Call_to_action from '.components/Call_to_action'
-import Contact from '.components/Contact'
-import Footer from '.components/Footer'
-import Masthead from '.components/Masthead'
-import Portfolio from '.components/Portfolio'
-import Services from '.components/Services'
+import Navigation from './components/Navigation.js'
+import About from './components/About.js'
+import Call_to_action from './components/Call_to_action.js'
+import Contact from './components/Contact.js'
+import Footer from './components/Footer.js'
+import Masthead from './components/Masthead.js'
+import Portfolio from './components/Portfolio.js'
+import Services from './components/Services.js'
 import logo from './logo.svg';
 import './App.css';
 
